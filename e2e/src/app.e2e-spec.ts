179b3042c1682b2bc44d3ddb7a5e93aa30e7dab9
josecortesdev <jocobe93@gmail.com>
2021-03-03 20:36:04 +0100

    commit 1

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to crudFRONT!');
+    expect(page.getTitleText()).toEqual('Welcome to FrontFintool!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/crudFRONT'),
+      dir: require('path').join(__dirname, './coverage/FrontFintool'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
